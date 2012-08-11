e10c9fa22efe385b5dd7fd26160a5750a19fa61f
Jens Kraemer <jk@jkraemer.net>
2012-08-11 15:23:57 +0200

    fixes problem with failing start date validation
    - occured when moving issues with a follows relationship that required the start
      date to be > Date.today on the task board

--- lib/backlogs_issue_patch.rb
+++ lib/backlogs_issue_patch.rb
@@ -108,6 +108,7 @@ module Backlogs
 
             self.fixed_version = self.story.fixed_version if self.story
             self.start_date = Date.today if self.start_date.blank? && self.status_id != IssueStatus.default.id
+            self.start_date = [Date.today, soonest_start].compact.max if self.start_date.blank? && self.status_id != IssueStatus.default.id
 
             self.tracker = Tracker.find(RbTask.tracker) unless self.tracker_id == RbTask.tracker
           elsif self.is_story? && Backlogs.setting[:set_start_and_duedates_from_sprint]

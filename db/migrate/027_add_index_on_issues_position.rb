305bb5bf2e44484c319092e15dc1b9ec516a1c19
Jan Schulz-Hofen <yeah@yeahrock.de>
2012-08-14 13:49:37 +0300

    only for planio: add default in order to play nice with code that does not have the column

--- db/migrate/027_add_index_on_issues_position.rb
+++ db/migrate/027_add_index_on_issues_position.rb
@@ -1,6 +1,7 @@
 class AddIndexOnIssuesPosition < ActiveRecord::Migration
   def self.up
     add_index :issues, :position 
+    change_column :issues, :position, :integer, :default => 0 
   end
 
   def self.down
